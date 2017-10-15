65b1e5a2f029bd8b2b28e516f94ed065561983a2
LJade <zly1615@qq.com>
2017-10-15 19:14:11 +0800

    首页  登录页

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function (app) {
+    /* GET home page. */
+    app.get('/', function(req, res, next) {
+        res.render('index');
+    });
+    app.get('/login', function(req, res, next) {
+        res.render('login/login');
+    });
+    app.get('/register', function(req, res, next) {
+        res.render('register/register');
+    });
+    app.get('/news_list', function(req, res, next) {
+        res.render('news/news_list');
+    });
+    app.get('/news_manage', function(req, res, next) {
+        res.render('news/news_manage');
+    });
+    app.get('/news_create', function(req, res, next) {
+        res.render('news/news_create');
+    });
+    app.get('/news_approve', function(req, res, next) {
+        res.render('news/news_approve');
+    });
+};
