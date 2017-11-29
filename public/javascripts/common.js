d5b20bef76523e32ba63a4161adc4ee241d1e0e7
LJade <zly1615@qq.com>
2017-11-29 23:40:59 +0800

    添加layui

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -3,3 +3,11 @@ window.onload = function () {
     wrapper.style.minHeight = document.body.clientHeight - wrapper.offsetHeight +'px';
 }
 
+layui.use('laydate', function(){
+    var laydate = layui.laydate;
+    //执行一个laydate实例
+    laydate.render({
+        elem: '#date',
+        theme: '#36a4e3'
+    });
+});
