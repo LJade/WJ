78ccd2d1496303cfaa98eea4fb75e614ce1ee2a8
xiaojun <z306223558@163.com>
2017-11-30 13:15:59 +0800

    修改下工单的路由

--- routes/order.js
+++ routes/order.js
@@ -3,22 +3,22 @@ var assert = require('./assert.js');
 
 var order_create = function(req, res, next) {
     var JADE_VAR = assert.getJADE();
-    res.render('order/order_create',JADE_VAR);
+    res.render('workOrder/order_create',JADE_VAR);
 };
 
 var order_approve = function(req, res, next) {
     var JADE_VAR = assert.getJADE();
-    res.render('order/order_approve',JADE_VAR);
+    res.render('workOrder/order_approve',JADE_VAR);
 };
 
 var order_mine = function(req, res, next) {
     var JADE_VAR = assert.getJADE();
-    res.render('order/order_mine',JADE_VAR);
+    res.render('workOrder/order_mine',JADE_VAR);
 };
 
 var order_detail = function(req, res, next) {
     var JADE_VAR = assert.getJADE();
-    res.render('order/order_detail',JADE_VAR);
+    res.render('workOrder/order_detail',JADE_VAR);
 };
 
 
