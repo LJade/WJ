a9e987a75e0304bb276ab52829ee652316b510b7
LJade <zly1615@qq.com>
2017-12-03 22:42:36 +0800

    用户选择

---
+++ public/javascripts/workflow/workflow_create.js
@@ -0,0 +1,16 @@
+var workflowCreate = function () {
+   var addUser = function () {
+        $('#addUser').on('click',function () {
+            
+        })
+   }
+
+
+
+    return {
+        init: function () {
+            addUser()
+        }
+    }
+}()
+workflowCreate.init()
