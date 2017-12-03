3b41a9d9a950ce06cd1ba3fd21305319c3bc4829
LJade <zly1615@qq.com>
2017-12-03 16:19:19 +0800

    流程配置加框

--- public/javascripts/workflow/workflow_config.js
+++ public/javascripts/workflow/workflow_config.js
@@ -1,15 +1,37 @@
 var workflowConfig = function () {
     var create = function () {
+        var originX = 20
+        var originY = 20
+        var id = 1
         $('#createNode').on('click',function () {
-            alert(2)
-            drawRec("新节点","20","20")
+            drawRec("新节点",originX,originY)
+            originX = originX > 860 ? 20 : originX + 120
+            originY = originX > 860 ? originY + 80 : originY;
+            id++
         })
         function drawRec(text,x,y) {
             this.rect=document.createElementNS("http://www.w3.org/2000/svg","rect")
             this.rect.setAttributeNS(null,"x",x)
             this.rect.setAttributeNS(null,"y",y)
-            this.rect.setAttributeNS(null,"width","120")
-            this.rect.setAttributeNS(null,"height","80")
+            this.rect.setAttributeNS(null,"width","90")
+            this.rect.setAttributeNS(null,"height","40")
+            this.rect.setAttributeNS(null,"rx","5")
+            this.rect.setAttributeNS(null,"ry","5")
+            this.rect.style.cssText = 'fill:white;stroke:black;stroke-width:2;'
+            this.rect.id = 'rect'+id
+            document.querySelector("svg").appendChild(this.rect)
+        }
+
+
+        function drawArrow(text,x,y) {
+            this.rect=document.createElementNS("http://www.w3.org/2000/svg","rect")
+            this.rect.setAttributeNS(null,"x",x)
+            this.rect.setAttributeNS(null,"y",y)
+            this.rect.setAttributeNS(null,"width","90")
+            this.rect.setAttributeNS(null,"height","40")
+            this.rect.setAttributeNS(null,"rx","5")
+            this.rect.setAttributeNS(null,"ry","5")
+            this.rect.style.cssText = 'fill:white;stroke:black;stroke-width:2;'
             document.querySelector("svg").appendChild(this.rect)
         }
     }
