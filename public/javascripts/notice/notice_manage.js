b12b662ddced469179ca95bf34e7073b9d2d50d7
LJade <zly1615@qq.com>
2017-12-12 23:52:20 +0800

    新加 新闻  公告的js 日期  修改首页跳转新窗口打开 修改最大宽度

---
+++ public/javascripts/notice/notice_manage.js
@@ -0,0 +1,19 @@
+var noticeManage = function () {
+    var dateInit = function () {
+        layui.use('laydate', function(){
+            var laydate = layui.laydate;
+            laydate.render({
+                elem: '#date',
+                theme: '#36a4e3'
+            });
+});
+    }
+
+    return {
+        init: function () {
+            dateInit()
+        }
+    }
+}();
+noticeManage.init();
+
